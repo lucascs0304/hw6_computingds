498e3f1203d8695e47c2fb87a5c355dfe785afbd
Renato Vassallo <rvs881@gmail.com>
2022-11-25 20:23:58 +0100

    Question 5 section 3

--- hw6.py
+++ hw6.py
@@ -113,13 +113,75 @@ class Deck:
 # 3. In this exercise you will create an interface that will serve as template 
 # for different figures to compute their perimeter and surface. 
 
+from abc import ABC, abstractmethod
+import math
+
 # 3.1Create an abstract class (interface) called "PlaneFigure" with two abstract methods:
 # compute_perimeter() that will implement the formula to compute the perimiter of the plane figure.
 # compute_surface() that will implement the formula to compute the surface of the plane figure.
 
-# 3.2 Create a child class called "Triangle" that inherits from "PlaneFigure" and has as parameters in the constructor "base", "c1", "c2", "h". ("base" being the base, "c1" and "c2" the other two sides of the triangle and "h" the height). Implement the abstract methods with the formula of the triangle.
+class PlaneFigure(ABC):
+    
+    @abstractmethod
+    def compute_perimeter(self):
+        pass
+    
+    @abstractmethod
+    def compute_surface(self):
+        pass
 
-# 3.3 Create a child class called "Rectangle" that inherits from "PlaneFigure" and has as parameters in the constructor "a", "b" (sides of the rectangle). Implement the abstract methods with the formula of the rectangle.
+# 3.2 Create a child class called "Triangle" that inherits from "PlaneFigure" and has as parameters 
+# in the constructor "base", "c1", "c2", "h". ("base" being the base, "c1" and "c2" the other two 
+# sides of the triangle and "h" the height). Implement the abstract methods with the formula of the triangle.
 
-# 3.3 Create a child class called "Circle" that inherits from "PlaneFigure" and has as parameters in the constructor "radius" (radius of the circle). Implement the abstract methods with the formula of the circle.
+class Triangle(PlaneFigure):
+    
+    def __init__(self, base, c1, c2, h):
+        self.base = base
+        self.c1 = c1
+        self.c2 = c2
+        self.height = h
+    
+    def compute_perimeter(self):
+        self.perimeter = self.base + self.c1 + self.c2
+        return self.perimeter
+    
+    def compute_surface(self):
+        self.surface = (self.base * self.height) / 2
+        return self.surface
+
+# 3.3 Create a child class called "Rectangle" that inherits from "PlaneFigure" and has as parameters in 
+# the constructor "a", "b" (sides of the rectangle). Implement the abstract methods with the formula of 
+# the rectangle.
+
+class Rectangle(PlaneFigure):
+    
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+    
+    def compute_perimeter(self):
+        self.perimeter = (self.a + self.b) * 2
+        return self.perimeter
+    
+    def compute_surface(self):
+        self.surface = self.a * self.b
+        return self.surface
+
+# 3.3 Create a child class called "Circle" that inherits from "PlaneFigure" and has as parameters in the 
+# constructor "radius" (radius of the circle). Implement the abstract methods with the formula of the circle.
+
+class Circle(PlaneFigure):
+    
+    def __init__(self, radius):
+        self.radius = radius
+    
+    def compute_perimeter(self):
+        self.perimeter = 2 * self.radius * math.pi 
+        return self.perimeter
+    
+    def compute_surface(self):
+        self.surface = math.pi * (self.radius)**2
+        return self.surface
+    
 
